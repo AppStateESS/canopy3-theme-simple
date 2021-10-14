feee245fc20d1ddfdaaf84c6deaf328bbda31261
Matt McNaney <mcnaneym@appstate.edu>
2021-10-14 13:07:24 -0400

    Removed jquery

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,8 +1,4 @@
 'use strict'
-/* global $, jQuery */
 import 'bootstrap'
 import '@fortawesome/fontawesome-free'
 import '@fortawesome/fontawesome-free/js/all.js'
-jQuery.fn.load = function (callback) {
-  $(window).on('load', callback)
-}
